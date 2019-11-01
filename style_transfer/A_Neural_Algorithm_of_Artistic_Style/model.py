57c7962326255265c041f9d1eb7179fb57cd0439
minoring <minho1350@gmail.com>
2019-11-01 13:58:47 -0700

    Implement training loops

--- style_transfer/A_Neural_Algorithm_of_Artistic_Style/model.py
+++ style_transfer/A_Neural_Algorithm_of_Artistic_Style/model.py
@@ -24,79 +24,77 @@ NORM_MEANS = np.array([103.939, 116.779, 123.68])  # VGG19 normalization mean.
 
 
 def neural_style():
-    """Create out model with access to intermediate layers.
+  """Create out model with access to intermediate layers.
 
     This function will load the VGG19 model and access the intermediate layers.
     These layers will then be used to create a new model that will take input
     image and return the outputs from these intermediate layers from the VGG model.
 
     Returns:
-        A keras model that take image inputs and outputs the style and
-        content intermediate layers.
+      A keras model that take image inputs and outputs the style and
+      content intermediate layers.
     """
-    # Load our model. We load pretrained VGG, trained on imagenet data
-    vgg = tf.keras.applications.vgg19.VGG19(include_top=False,
-                                            weights='imagenet')
-    vgg.trainable = False
-    # Get output layers corresponding to style and content layers
-    style_outputs = [vgg.get_layer(name).output for name in STYLE_LAYERS]
-    content_outputs = [vgg.get_layer(name).output for name in CONTENT_LAYERS]
-    model_outputs = style_outputs + content_outputs
-    # Build model
-    return tf.keras.models.Model(vgg.input, model_outputs)
+  # Load our model. We load pretrained VGG, trained on imagenet data
+  vgg = tf.keras.applications.vgg19.VGG19(include_top=False, weights='imagenet')
+  vgg.trainable = False
+  # Get output layers corresponding to style and content layers
+  style_outputs = [vgg.get_layer(name).output for name in STYLE_LAYERS]
+  content_outputs = [vgg.get_layer(name).output for name in CONTENT_LAYERS]
+  model_outputs = style_outputs + content_outputs
+  # Build model
+  return tf.keras.models.Model(vgg.input, model_outputs)
 
 
 def compute_losses(model, gen_image, org_style_reprs, org_content_reprs):
-    """This function will compute the total loss.
+  """This function will compute the total loss.
 
-    Args:
-      model: The model that will give us access to the intermediate layers
-      gen_image: Image we are generating. This is what we are updating with our 
-        optimization process. We apply the gradients the loss wrt this image.
-      org_style_reprs: Precomputed gram metrices corresponding to the defined
-        style layers of interest.
-      org_content_reprs: Precomputed output from defined content layers 
-          of interest.
+  Args:
+    model: The model that will give us access to the intermediate layers
+    gen_image: Image we are generating. This is what we are updating with our 
+      optimization process. We apply the gradients the loss wrt this image.
+    org_style_reprs: Precomputed gram metrices corresponding to the defined
+      style layers of interest.
+    org_content_reprs: Precomputed output from defined content layers 
+        of interest.
     
     Returns:
       Dictionary of {'total_loss', 'style_loss', 'content_loss'}
     """
-    # Feed our generating image through our model. This will give us the content
-    # and style representations at our desired layers. Since we're using
-    # eager execution, our model is callable just like any other function
-    model_outputs = model(gen_image)
+  # Feed our generating image through our model. This will give us the content
+  # and style representations at our desired layers. Since we're using
+  # eager execution, our model is callable just like any other function
+  model_outputs = model(gen_image)
 
-    gen_style_reprs = model_outputs[:NUM_STYLE_LAYERS]
-    gen_content_reprs = model_outputs[
-        NUM_STYLE_LAYERS:]  # TODO(minoring): 개선 법 없을까
-    # Style이 먼저나오고 content가 먼저나오는데 이 순서가 중요함. 헷갈릴것 같은데
+  gen_style_reprs = model_outputs[:NUM_STYLE_LAYERS]
+  gen_content_reprs = model_outputs[
+      NUM_STYLE_LAYERS:]  # TODO(minoring): 개선 법 없을까
+  # Style이 먼저나오고 content가 먼저나오는데 이 순서가 중요함. 헷갈릴것 같은데
 
-    style_loss = 0
-    content_loss = 0
+  style_loss = 0
+  content_loss = 0
 
-    # Accumulate style losses from all layers
-    for org_style_repr, gen_style_repr in zip(org_style_reprs,
-                                              gen_style_reprs):
-        style_loss += WEIGHT_PER_STYLE_LAYER * compute_style_loss(
-            org_style_repr, gen_style_repr[0])
+  # Accumulate style losses from all layers
+  for org_style_repr, gen_style_repr in zip(org_style_reprs, gen_style_reprs):
+    style_loss += WEIGHT_PER_STYLE_LAYER * compute_style_loss(
+        org_style_repr, gen_style_repr[0])
 
-    # Accumulate content losses from all layers
-    for org_content_repr, gen_content_repr in zip(org_content_reprs,
-                                                  gen_content_reprs):
-        content_loss += WEIGHT_PER_CONTENT_LAYER * compute_content_loss(
-            org_content_repr, gen_content_repr[0])
+  # Accumulate content losses from all layers
+  for org_content_repr, gen_content_repr in zip(org_content_reprs,
+                                                gen_content_reprs):
+    content_loss += WEIGHT_PER_CONTENT_LAYER * compute_content_loss(
+        org_content_repr, gen_content_repr[0])
 
-    # Get total loss
-    total_loss = STYLE_WEIGHT * style_loss + CONTENT_WEIGHT * content_loss
-    return dict(total_loss=total_loss,
-                style_loss=style_loss,
-                content_loss=content_loss)
+  # Get total loss
+  total_loss = STYLE_WEIGHT * style_loss + CONTENT_WEIGHT * content_loss
+  return dict(total_loss=total_loss,
+              style_loss=style_loss,
+              content_loss=content_loss)
 
 
 def compute_content_loss(org_content_repr, gen_content_repr):
-    """Compute content loss by calculating Euclidian distance between 
-       intermediate content representation of original image and 
-       image that is generated.
+  """Compute content loss by calculating Euclidian distance between 
+     intermediate content representation of original image and 
+     image that is generated.
 
     Args:
       org_content_repr: Output of intermediate feature activation when 
@@ -107,11 +105,11 @@ def compute_content_loss(org_content_repr, gen_content_repr):
     Returns:
       Loss between feature representation of original image and generated image.
     """
-    return tf.reduce_sum(tf.square(org_content_repr - gen_content_repr))
+  return tf.reduce_sum(tf.square(org_content_repr - gen_content_repr))
 
 
 def compute_style_loss(org_style_repr, gen_style_repr):
-    """Compute style loss between style representation of original image and
+  """Compute style loss between style representation of original image and
     generated image
 
     Args:
@@ -124,27 +122,26 @@ def compute_style_loss(org_style_repr, gen_style_repr):
       Style loss.
     """
 
-    h, w, c = org_style_repr.get_shape().as_list()
-    gram_original = _gram_matrix(org_style_repr)
-    gram_generated = _gram_matrix(gen_style_repr)
+  h, w, c = org_style_repr.get_shape().as_list()
+  gram_original = _gram_matrix(org_style_repr)
+  gram_generated = _gram_matrix(gen_style_repr)
 
-    return (tf.reduce_sum(
-        tf.square(gram_original - gram_generated) / (4. * (c**2) *
-                                                     (w * h)**2)))
+  return (tf.reduce_sum(
+      tf.square(gram_original - gram_generated) / (4. * (c**2) * (w * h)**2)))
 
 
 def _gram_matrix(input_tensor):
-    """Calculate gram matrix which is (channel, channel) shape.
+  """Calculate gram matrix which is (channel, channel) shape.
     Args:
       input_tensor: tensor that is calculated.
 
     Returns:
       gram matrix of input tensor.
     """
-    # Now we have channels first image
-    channels = int(input_tensor.shape[-1])
-    # Convert into channel last tesnro
-    a = tf.reshape(input_tensor, (-1, channels))
-    filter_dims = tf.shape(a)[0]
-    gram = tf.matmul(a, a, transpose_a=True)
-    return gram / tf.cast(filter_dims, tf.float32)
+  # Now we have channels first image
+  channels = int(input_tensor.shape[-1])
+  # Convert into channel last tesnro
+  a = tf.reshape(input_tensor, (-1, channels))
+  filter_dims = tf.shape(a)[0]
+  gram = tf.matmul(a, a, transpose_a=True)
+  return gram / tf.cast(filter_dims, tf.float32)
--- style_transfer/A_Neural_Algorithm_of_Artistic_Style/utils.py
+++ style_transfer/A_Neural_Algorithm_of_Artistic_Style/utils.py
@@ -4,43 +4,50 @@ from __future__ import division
 from __future__ import print_function
 
 from absl import flags
+# from dataset import deprocess_img
 
 import matplotlib.pyplot as plt
-import numpy as np
-
-
-def define_flags():
-    flags.DEFINE_integer('num_training_steps', 1000,
-                         'The number of training steps for neural style')
+import matplotlib as mpl
+import numpy as np 
+from PIL import Image
 
 
 def log_training_info(plot_img, losses, step, elapsed_time):
-    imshow(plot_img, title='Generated image')
-    print('Steps: {}'.format(step))
-    print('total loss: {:.4e}, '.format(losses['total_loss']),
-          'style loss: {:.4e}, '.format(losses['style_loss']),
-          'content loss: {:.4e}, '.format(losses['content_loss']),
-          'time: {:.4f}s'.format(elapsed_time))
+  imshow(plot_img, title='Generated image')
+  print('Steps: {}'.format(step))
+  print('total loss: {:.4e}, '.format(losses['total_loss']),
+        'style loss: {:.4e}, '.format(losses['style_loss']),
+        'content loss: {:.4e}, '.format(losses['content_loss']),
+        'time: {:.4f}s'.format(elapsed_time))
 
 
 def imshow(img, title=None):
-    """Show image.
+  """Show image.
 
-    Args:
-      img: numpy array of image of (batch_size, width or height, width or height)
-    """
-    # Remove the batch dimension.
-    out = np.squeeze(img, axis=0)
-    # Normalize for display.
-    out = out.astype('uint8')
-    if title is not None:
-        plt.title(title)
-    plt.imshow(out)
+  Args:
+    img: numpy array of image of (batch_size, width or height, width or height)
+    title: String of title of image.
+  """
+  import IPython.display as display
+  mpl.rcParams['figure.figsize'] = (12,12)
+  mpl.rcParams['axes.grid'] = False
+  display.clear_output(wait=True)
+  display.display(Image.fromarray(img))
+
+  # fig = plt.figure()
+  # print('img shape:', img.shape)
+  # # Normalize for display.
+  # img = img.astype('uint8')
+  # if title is not None:
+  #   plt.title(title)
+  # plt.imshow(img)
+  # fig.canvas.draw_idle()
+  # plt.pause(.001)
 
 
 def load_model():
-    pass
+  pass
 
 
 def save_model():
-    pass
+  pass
